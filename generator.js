7d291947d56766b4fcff903d1f2d5f242febac68
TaeBbong <mok05289@korea.ac.kr>
2021-12-28 01:25:37 +0900

    [ADD] Padding 추가

--- generator.js
+++ generator.js
@@ -2,13 +2,14 @@ $(document).ready(function () {
   var $projects_div = $(".projects");
 
   $.getJSON("https://taebbong.github.io/resume/projects.json", function (json) {
-    var i = 0;
     json.forEach((data) => {
-      i += 1;
       var $elem = $("#project-template").clone().removeAttr("id");
-      $elem.find(".project-name").html("프로젝트#" + i + "<br />" + data.name);
+      $elem
+        .find(".project-name")
+        .html("프로젝트 #" + data.id + "<br />" + data.name);
       $elem.find("#project-period").html("제작기간 : " + data.period);
       $elem.find("#project-tech").html("사용기술 : " + data.tech.join(", "));
+
       $elem
         .find("#project-repo")
         .html(
@@ -18,9 +19,11 @@ $(document).ready(function () {
         .find("#project-link")
         .html("서비스 : " + "<a href=" + data.link + ">" + data.link + "</a>");
       $elem.find("#project-image").attr("src", data.img);
-      $elem.find("#project-desc").html("설명 : " + data.desc);
-      $elem.find("#project-wil").html("배운 것 : " + data.wil);
-      $elem.find("#project-problem").html("어려웠던 것 : " + data.problem);
+      $elem.find("#project-desc").html("<b>설명</b> : " + data.desc);
+      $elem.find("#project-wil").html("<b>배운 것</b> : " + data.wil);
+      $elem
+        .find("#project-problem")
+        .html("<b>어려웠던 것</b> : " + data.problem);
       $projects_div.append($elem);
     });
   });
