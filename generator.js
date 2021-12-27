ef9d49aeaebb5b93fcd5ff2b86bbddbeeb7bd3cd
TaeBbong <mok05289@korea.ac.kr>
2021-12-28 01:01:12 +0900

    [ADD] 프로젝트 템플릿 반영

--- generator.js
+++ generator.js
@@ -7,11 +7,20 @@ $(document).ready(function () {
       i += 1;
       var $elem = $("#project-template").clone().removeAttr("id");
       $elem.find(".project-name").html("프로젝트#" + i + "<br />" + data.name);
-      $elem.find("#project-tech").html(data.desc);
-      $elem.find("#project-link").html(data.link);
+      $elem.find("#project-period").html("제작기간 : " + data.period);
+      $elem.find("#project-tech").html("사용기술 : " + data.tech.join(", "));
+      $elem
+        .find("#project-repo")
+        .html(
+          "소스코드 : " + "<a href=" + data.repo + ">" + data.repo + "</a>"
+        );
       $elem
         .find("#project-link")
-        .html("<a href=" + data.link + ">" + data.link + "</a>");
+        .html("서비스 : " + "<a href=" + data.link + ">" + data.link + "</a>");
+      $elem.find("#project-image").attr("src", data.img);
+      $elem.find("#project-desc").html("설명 : " + data.desc);
+      $elem.find("#project-wil").html("배운 것 : " + data.wil);
+      $elem.find("#project-problem").html("어려웠던 것 : " + data.problem);
       $projects_div.append($elem);
     });
   });
