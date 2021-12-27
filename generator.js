37b1d79c761fe399901a1304c74ee5a81af8ebcd
TaeBbong <mok05289@korea.ac.kr>
2021-12-27 22:09:18 +0900

    [ADD] jquery 활용 JSON 데이터 연결, 템플릿 적용

--- generator.js
+++ generator.js
@@ -1,9 +1,20 @@
 $(document).ready(function () {
+  var $projects_div = $(".projects");
+
+  $.getJSON("https://taebbong.github.io/resume/projects.json", function (json) {
+    var i = 0;
+    json.forEach((data) => {
+      i += 1;
+      var $elem = $("#project-template").clone().removeAttr("id");
+      $elem.find(".project-name").html("프로젝트#" + i + "<br />" + data.name);
+      $elem.find("#project-tech").html(data.desc);
+      $elem.find("#project-link").html(data.link);
+      $elem
+        .find("#project-link")
+        .html("<a href=" + data.link + ">" + data.link + "</a>");
+      $projects_div.append($elem);
+    });
+  });
+
   $("#wrap-toc").toc({ content: ".wrap-content", headings: "h2,h3,h4" });
-  $.getJSON(
-    "https://taebbong.github.io/resume/projects.json?callback=?",
-    function (data) {
-      console.log(data);
-    }
-  );
 });
